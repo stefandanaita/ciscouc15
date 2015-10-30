bbcedb381ca4e6ca3b937ca90df886a3cbd09759
Stefan Danaita <stefan.danaita@gmail.com>
2015-10-30 03:58:07 +0000

    Maps & Calendar

--- source/resources/views/dashboard.blade.php
+++ source/resources/views/dashboard.blade.php
@@ -20,6 +20,174 @@
 
     <link rel="stylesheet" href="<?php echo asset('css/fa-animated.min.css')?>" type="text/css">
     <link rel="stylesheet" href="<?php echo asset('css/style.css')?>" type="text/css">
+
+    <meta name="author" content="Andreea-Camelia-Simona Bertea">
+  	<script src="https://maps.googleapis.com/maps/api/js"></script>
+
+    <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+
+
+
+
+
+
+    <script type="text/javascript">
+      function showCurrentLocation(position)
+      {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var coords = new google.maps.LatLng(latitude, longitude);
+
+        var mapOptions = {
+          zoom: 15,
+          center: coords,
+          mapTypeControl: true,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        //create the map, and place it in the HTML map div
+        map = new google.maps.Map(document.getElementById("mapPlaceholder"), mapOptions);
+
+        //place the initial marker
+        var marker = new google.maps.Marker({
+          position: coords,
+          map: map,
+          title: "You are here!"
+        });
+      }
+
+      if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showCurrentLocation);
+      } else {
+        alert("Geolocation API is not supported.");
+      }
+
+
+      // Your Client ID can be retrieved from your project in the Google
+      // Developer Console, https://console.developers.google.com
+      var CLIENT_ID = '470495590508-93e91fhc6l2sp53romjs1q061q25jfi6.apps.googleusercontent.com';
+
+      var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+      /**
+       * Check if current user has authorized this application.
+       */
+      function checkAuth() {
+        gapi.auth.authorize(
+          {
+            'client_id': CLIENT_ID,
+            'scope': SCOPES.join(' '),
+            'immediate': true
+          }, handleAuthResult);
+      }
+
+      /**
+       * Handle response from authorization server.
+       *
+       * @param {Object} authResult Authorization result.
+       */
+      function handleAuthResult(authResult) {
+        var authorizeDiv = document.getElementById('authorize-div');
+        if (authResult && !authResult.error) {
+          // Hide auth UI, then load client library.
+          authorizeDiv.style.display = 'none';
+          loadCalendarApi();
+        } else {
+          // Show auth UI, allowing the user to initiate authorization by
+          // clicking authorize button.
+          authorizeDiv.style.display = 'inline';
+        }
+      }
+
+      /**
+       * Initiate auth flow in response to user clicking authorize button.
+       *
+       * @param {Event} event Button click event.
+       */
+      function handleAuthClick(event) {
+        gapi.auth.authorize(
+          {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+          handleAuthResult);
+        return false;
+      }
+
+      /**
+       * Load Google Calendar client library. List upcoming events
+       * once client library is loaded.
+       */
+      function loadCalendarApi() {
+        gapi.client.load('calendar', 'v3', listUpcomingEvents);
+      }
+
+      /**
+       * Print the summary and start datetime/date of the next ten events in
+       * the authorized user's calendar. If no events are found an
+       * appropriate message is printed.
+       */
+      function listUpcomingEvents() {
+        var request = gapi.client.calendar.events.list({
+          'calendarId': 'primary',
+          'timeMin': (new Date()).toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'maxResults': 10,
+          'orderBy': 'startTime'
+        });
+
+        request.execute(function(resp) {
+          var events = resp.items;
+          appendPre('Upcoming events:');
+
+          if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+              var event = events[i];
+              var when = event.start.dateTime;
+              if (!when) {
+                when = event.start.date;
+              }
+              appendPre(event.summary + ' (' + when + ')')
+
+              if (event.location != undefined) {
+                var geocoder = new google.maps.Geocoder();
+                geocodeAddress(event.location, geocoder, map);
+              }
+            }
+          } else {
+            appendPre('No upcoming events found.');
+          }
+
+        });
+      }
+
+      function geocodeAddress(address, geocoder, resultsMap) {
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === google.maps.GeocoderStatus.OK) {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
+
+      /**
+       * Append a pre element to the body containing the given message
+       * as its text node.
+       *
+       * @param {string} message Text to be placed in pre element.
+       */
+      function appendPre(message) {
+        var pre = document.getElementById('output');
+        var textContent = document.createTextNode(message + '\n');
+        pre.appendChild(textContent);
+      }
+
+    </script>
+    <script src="https://apis.google.com/js/client.js?onload=checkAuth">
+    </script>
   </head>
   <body>
     <div class="container menubar">
@@ -33,5 +201,27 @@
         </div>
       </div>
     </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div id="mapPlaceholder"></div>
+        </div>
+
+        <div class="col-lg-4">
+          <div id="authorize-div" style="display: none">
+            <span>Authorize access to Google Calendar API</span>
+            <!--Button for the user to click to initiate auth sequence -->
+            <button id="authorize-button" onclick="handleAuthClick(event)">
+              Authorize
+            </button>
+          </div>
+          <pre id="output"></pre>
+        </div>
+
+        <div class="col-lg-4">
+
+        </div>
+      </div>
   </body>
 </html>
